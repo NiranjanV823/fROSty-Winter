d10dd8d1b1d5ebdbdcbea9af9eeea6048c37adb4
Niranjan <niranjanverma823@gmail.com>
2023-12-24 00:18:35 +0530

    task_1 models added

--- src/task_1/setup.py
+++ src/task_1/setup.py
@@ -13,8 +13,10 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
                 (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', 'mrs_hudson_world.launch.py'))),
-                        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', 'mrs_hudson.rviz'))),
-                                (os.path.join('share', package_name, 'worlds'), glob(os.path.join('worlds', 'mrs_hudson_arena.sdf'))),
+                (os.path.join('share', package_name, 'config'), glob(os.path.join('config', 'mrs_hudson.rviz'))),
+                (os.path.join('share', package_name, 'worlds'), glob(os.path.join('worlds', 'mrs_hudson_arena.sdf'))),
+                (os.path.join('share', package_name, 'models'), glob(os.path.join('models', 'mrs_hudson.urdf'))),
+
 
 
 
